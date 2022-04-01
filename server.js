77e39b3e7311b32a694bb98a5540c57b912bfb10
jamav <jamav@DESKTOP-GINU2UV>
2022-03-31 23:37:55 -0300

    ADS

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server    
+const path = require('path');
 const express = require('express');
+const app = express();
 
-const path = require('path');   
+// Serve static files
+app.use(express.static(__dirname + '/dist/clase1'));
 
-const app = express();   
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/clase1/index.html'));
+});
 
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist/'));
-
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
-});  
-
-// Start the app by listening on the default Heroku port    
+// default Heroku port
 app.listen(process.env.PORT || 8080);
